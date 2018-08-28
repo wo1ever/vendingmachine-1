3091891354ce32cf17b33121d6d94428764e31ea
wo1ever <seyomiii@gmail.com>
2018-08-28 10:40:13 +0900

    테스트케이스 한글화

--- test_vm.py
+++ test_vm.py
@@ -1,21 +1,88 @@
 from vm import VendingMachine
 
 
-def test_initial_change_should_be_zero():
-    m = VendingMachine()
-    assert "잔액은 0원입니다" == m.run("잔액")
+def test_초기_잔액은_0원():
+   m = VendingMachine()
+   assert “잔액은 0원입니다” == m.run(“잔액“)
 
-def test_insert_coin_and_check():
-    m = VendingMachine()
-    assert "100원을 넣었습니다" == m.run("동전 100")
-    assert "잔액은 100원입니다" == m.run("잔액")
+def test_동전_넣고_잔액_검사():
+   m = VendingMachine()
+   assert “100원을 넣었습니다” == m.run(“동전 100")
+   assert “잔액은 100원입니다” == m.run(“잔액“)
 
-def test_accumulation_of_change():
-    m = VendingMachine()
-    m.run("동전 100")
-    m.run("동전 100")
-    assert "잔액은 200원입니다" == m.run("잔액")
+def test_잔액_누적():
+   m = VendingMachine()
+   m.run(“동전 100”)
+   m.run(“동전 100")
+   assert “잔액은 200원입니다” == m.run(“잔액“)
+
+def test_음료_뽑기():
+   m = VendingMachine()
+   m.run(“동전 500”)
+   assert “커피가 나왔습니다” == m.run(“음료 커피“)
+   assert “잔액은 350원입니다” == m.run(“잔액“)
+
+def test_모르는_음료_뽑기():
+   m = VendingMachine()
+   m.run(“동전 500”)
+   assert “알 수 없는 음료입니다” == m.run(“음료 맥주“)
+   assert “잔액은 500원입니다” == m.run(“잔액“)
+
+def test_동전이_부족한_상황에서_음료_뽑기():
+   m = VendingMachine()
+   m.run(“동전 100”)
+   assert “잔액이 부족합니다” == m.run(“음료 커피“)
+   assert “잔액은 100원입니다” == m.run(“잔액“)
 
 def test_unknown_command():
-    m = VendingMachine()
-    assert "알 수 없는 명령입니다" == m.run("웅앵")
+   m = VendingMachine()
+   assert “알 수 없는 명령입니다” == m.run(“웅앵“)
+
+def test_valid_coins():
+   m = VendingMachine()
+   valid_coins = [“10”, “50", “100”, “500"]
+   for coin in valid_coins:
+       assert coin + “원을 넣었습니다” == m.run(“동전 ” + coin)
+
+def test_10():
+   m = VendingMachine()
+   assert “10원을 넣었습니다” == m.run(“동전 10")
+
+def test_50():
+   m = VendingMachine()
+   assert “50원을 넣었습니다” == m.run(“동전 50")
+
+def test_100():
+   m = VendingMachine()
+   assert “100원을 넣었습니다” == m.run(“동전 100")
+
+def test_500():
+   m = VendingMachine()
+   assert “500원을 넣었습니다” == m.run(“동전 500")
+
+def test_invalid_coin():
+   m = VendingMachine()
+   assert “알 수 없는 동전입니다” == m.run(“동전 999")
+
+# from vm import VendingMachine
+#
+# # 최대한 쪼개서 해야 한다. 하나의 def 안에 첫번째 assert가 깨지면 아래것은 테스트를 못하기 때문이다.
+#
+# def test_initial_change_should_be_zero():
+#     m = VendingMachine()  # 벤딩머신의 새로운 인스턴스가 생긴다. (VendingMachine은 개념이다. 이것의 실체인 인스턴스를 만들어놓고 써야 한다. 그냥 VendingMachine.run 은 쓸 수 없다. )
+#     assert “잔액은 0원입니다” == m.run(“잔액“)
+#
+# def test_insert_coin_and_check():
+#     m = VendingMachine()
+#     assert “100원을 넣었습니다” == run(“동전 100")
+#     assert “잔액은 100원입니다” == run(“잔액“)
+#
+# def test_accumulation_of_change():
+#     m = VendingMachine()
+#     run(“동전 100”)
+#     run(“동전 100")
+#     assert “잔액은 200원입니다” == run(“잔액“)
+#
+# def test_mola():
+#     m = VendingMachine()
+#     assert “알 수 없는 명령 입니다” == run(“우애웅“)
--- vm.py
+++ vm.py
@@ -1,16 +1,64 @@
 class VendingMachine:
-    def __init__(self):
-        self._change = 0
+   def __init__(self):
+       self._change = 0
 
-    def run(self, raw):
-        tokens = raw.split(" ")
-        cmd, params = tokens[0], tokens[1:]
+   def run(self, raw):
+       tokens = raw.split(” “)
+       cmd, params = tokens[0], tokens[1:]
 
-        if cmd == "잔액":
-            return "잔액은 " + str(self._change) + "원입니다"
-        elif cmd == "동전":
-            coin = params[0]
-            self._change += int(coin)
-            return coin + "원을 넣었습니다"
-        else:
-            return "알 수 없는 명령입니다"
+       # if 와 elif는 중간에 띄어쓰지 않는다.
+
+       if cmd == “잔액“:
+           return “잔액은 ” + str(self._change) + “원입니다”
+       elif cmd == “동전“:
+           # known_coins = [“10”, “50", “100”, “500"]
+           known_coins = “10,50,100,500”.split(“,”)
+
+           coin = params[0]
+           if coin not in known_coins:
+               return “알 수 없는 동전입니다”
+           self._change += int(coin)
+           return coin + “원을 넣었습니다”
+
+       elif cmd == “음료“:
+           known_beverage = “커피”
+           price = 150
+
+           beverage = params[0]
+           if beverage != known_beverage:
+               return “알 수 없는 음료입니다”
+           if self._change < price:
+               return “잔액이 부족합니다”
+           self._change = self._change - price
+           return beverage + “가 나왔습니다”
+       else:
+           return “알 수 없는 명령입니다”
+
+
+
+# class VendingMachine:  # 타입을 만든다 # 메세지는 인스턴스에게 보내진다.
+#     def __init__(self):  # 이닛은 벤딩머신 클래스가 호출되는 순간 실행된다. self는 지금 만들어진 인스턴스를 말함
+#         self._change = 0  # VendingMachine  에 change라는 변수가 생기고 값이 0이다.
+#                           # 중간에 _ 이 들어가면 쓰지 말라는 럿임
+#
+#     def run(self, raw):  # v1 첫번째 벤딩머신이 self로 담기고, 동전 100 이 raw에 담긴다.
+#
+#         tokens = raw.split(” “)
+#         cmd, params = tokens[0], tokens[1:]
+#
+#         if cmd == “잔액“:
+#             return “잔액은 ” + str(self._change) + “원입니다”
+#
+#         elif cmd == “동전“:
+#             coin = params[0]
+#             self._change = int(coin) + change
+#             return coin + “원을 넣었습니다”
+#
+#         else :
+#             return “알 수 없는 명령 입니다”
+
+       # 인터페이스로 바꾸거나 번역할때 a 와 b 가 같은것으로 처리되는가? 그렇다면 중복이다!
+
+# v1 = VendingMachine()
+# v2 = VendingMachine()
+# v1.run(“동전 100”)
